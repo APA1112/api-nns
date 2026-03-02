a992b7d731dde635541eef23711c4258a8cbd77a
APA1112 <alejandropatonaraujo2@gmail.com>
2026-03-02 12:01:17 +0100

    feat: Add validation to Client creation endpoint in ClientController

--- src/Controller/Api/ClientController.php
+++ src/Controller/Api/ClientController.php
@@ -9,6 +9,7 @@ use Symfony\Component\Routing\Attribute\Route;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 final class ClientController extends AbstractController
 {
@@ -20,10 +21,18 @@ final class ClientController extends AbstractController
     }
 
     #[Route('/api/clients', name: 'api_clients_create', methods: ["POST"])]
-    public function create(Request $request, SerializerInterface $serializer, EntityManagerInterface $entityManager): JsonResponse
+    public function create(Request $request, SerializerInterface $serializer, EntityManagerInterface $entityManager, ValidatorInterface $validator): JsonResponse
     {
         $content = $request->getContent();
         $client = $serializer->deserialize($content, Client::class, 'json');
+        $errors = $validator->validate($client);
+        if (count($errors) > 0) {
+            $error_messages = [];
+            foreach ($errors as $error) {
+                $error_messages[$error->getPropertyPath()][] = $error->getMessage();
+            }  
+            return $this->json($error_messages, 400);
+        }
         $entityManager->persist($client);
         $entityManager->flush();
         return $this->json($client, 201);
