c697987c065e5629cef3a53028c5aa13e65b883d
Alejandro <alejandropatonaraujo2@gmail.com>
2026-03-10 21:28:20 +0100

    feat: adding UserController

---
+++ src/Controller/Api/UserController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Controller\Api;
+
+use App\Entity\User;
+use App\Entity\Role;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use OpenApi\Attributes as OA;
+use Nelmio\ApiDocBundle\Attribute\Model;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\HttpFoundation\Response;
+
+#[Route('/api/users')]
+final class UserController extends AbstractController
+{
+    #[Route('', methods: ['POST'])]
+    // #[IsGranted('ROLE_ADMIN')] // Solo admins pueden crear
+    public function create(
+        Request $request, 
+        EntityManagerInterface $em, 
+        UserPasswordHasherInterface $hasher
+    ): JsonResponse {
+        $data = json_decode($request->getContent(), true);
+
+        $user = new User();
+        $user->setEmail($data['email']);
+        
+        // Hashear el password antes de guardar
+        $hashedPassword = $hasher->hashPassword($user, $data['password']);
+        $user->setPassword($hashedPassword);
+
+        // Aquí deberías buscar la entidad Role y asignarla
+        // $user->setUserRole($roleEntity);
+
+        $em->persist($user);
+        $em->flush();
+
+        return $this->json(['message' => 'Usuario creado'], Response::HTTP_CREATED);
+    }
+
+    #[Route('/{id}', methods: ['PUT'])]
+    public function update(
+        User $user, 
+        Request $request, 
+        EntityManagerInterface $em, 
+        UserPasswordHasherInterface $hasher
+    ): JsonResponse {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN'); // O validar si es el mismo usuario
+        
+        $data = json_decode($request->getContent(), true);
+
+        if (isset($data['email'])) $user->setEmail($data['email']);
+        if (isset($data['password'])) {
+            $user->setPassword($hasher->hashPassword($user, $data['password']));
+        }
+
+        $em->flush();
+        return $this->json(['message' => 'Usuario actualizado']);
+    }
+
+    #[Route('/{id}', methods: ['DELETE'])]
+    //#[IsGranted('ROLE_ADMIN')]
+    public function delete(User $user, EntityManagerInterface $em): JsonResponse
+    {
+        $em->remove($user);
+        $em->flush();
+        return $this->json(null, Response::HTTP_NO_CONTENT);
+    }
+}
